20e1da83983b9ab1f5dd3c63a0fc5b6412b5c9aa
RomanDovgii <romandovgiiwork@gmail.com>
2020-05-22 15:54:37 +0300

    fix for small button

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -1,10 +1,10 @@
-const openMenuButton = document.querySelector(`.functions__button`);
-const header = document.querySelector(`.header`);
-const mobileMenu = document.querySelector(`.information`);
+const openMenuButton = document.querySelector(".functions__button");
+const header = document.querySelector(".header");
+const mobileMenu = document.querySelector(".information");
 
-openMenuButton.addEventListener(`click`,() => {
-  if (openMenuButton.style.display !== `none`) {
-    mobileMenu.style.display = `block`;
-    header.style.backgroundColor = `#ffffff`;
+openMenuButton.addEventListener("click", function() {
+  if (openMenuButton.style.display !== "none") {
+    mobileMenu.style.display = "block";
+    header.style.backgroundColor = "#ffffff";
   }
 });
